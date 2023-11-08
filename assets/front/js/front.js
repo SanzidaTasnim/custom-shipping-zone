06bb43164cd170ea6359fad71347009d6ac95675
SanzidaTasnim <tasnim5sanzida@gmail.com>
2023-11-08 14:21:11 +0600

    some change detected

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -6,35 +6,39 @@ jQuery(function($){
     
         zipInput.blur(function () {
             const enteredZipCode = zipInput.val();
-            $.ajax({
-                url: AJAX_OBJECT.ajax_url,
-                type: 'POST',
-                data: { 
-                    action: 'cbs_shipping_zone_verify',
-                    zipCode: enteredZipCode 
-                },
-                dataType: 'JSON',
-                success: function (response) {
-                    checkoutButton.prop('disabled', false);
+            if( zipInput.val() ) {
+                $.ajax({
+                    url: AJAX_OBJECT.ajax_url,
+                    type: 'POST',
+                    data: { 
+                        action: 'cbs_shipping_zone_verify',
+                        zipCode: enteredZipCode 
+                    },
+                    dataType: 'JSON',
+                    success: function (response) {
+                        checkoutButton.prop('disabled', false);
 
-                    if ( response.data.allowedZip.includes(response.data.zipcode)  ) {
-                        Swal.fire({
-                          title: "Shipping Zone Valid!",
-                          text: "FREE!",
-                          icon: "success"
-                        });
-                    } else {
-                        Swal.fire({
-                          title: "Shipping Zone Invalid!",
-                          text: "Try different shipping zone",
-                          icon: "error"
-                        });
+                        if ( response.data.allowedZip.includes(response.data.zipcode)  ) {
+                            Swal.fire({
+                              title: "Shipping Zone Valid!",
+                              text: "FREE!",
+                              icon: "success"
+                            });
+                        } else {
+                            Swal.fire({
+                              title: "Shipping Zone Invalid!",
+                              text: "Try different shipping zone",
+                              icon: "error"
+                            });
+                            zipInput.val('');
+                        }
+                    },
+                    error: function () {
+                        console.log ("Error: Unable to validate ZIP code.");
                     }
-                },
-                error: function () {
-                    console.log ("Error: Unable to validate ZIP code.");
-                }
-            });
+                });
+            }
+           
 
         });
     });   
